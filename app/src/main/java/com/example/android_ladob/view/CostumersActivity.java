9be3a1a847ed7a9bf879ee4e6a0545cb677a682c
Joyce <joyce_barros@yahoo.com.br>
2020-11-12 18:17:32 -0300

    Criando layout activity_costumers.xml e Activitys.

--- app/src/main/java/com/example/android_ladob/view/CostumersActivity.java
+++ app/src/main/java/com/example/android_ladob/view/CostumersActivity.java
@@ -2,15 +2,46 @@ package com.example.android_ladob.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.example.android_ladob.R;
 
 public class CostumersActivity extends AppCompatActivity {
 
+    private LinearLayout linearLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_costumers);
+
+        linearLayout = findViewById(R.id.llProducts);
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CostumersActivity.this, ProductsActivity.class);
+                startActivity(intent);
+            }
+        });
+        linearLayout = findViewById(R.id.llOrder);
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CostumersActivity.this, OrderActivity.class);
+                startActivity(intent);
+            }
+        });
+        linearLayout = findViewById(R.id.llPagamento);
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CostumersActivity.this, OrderActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/android_ladob/view/PagamentoActivity.java
@@ -0,0 +1,16 @@
+package com.example.android_ladob.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.android_ladob.R;
+
+public class PagamentoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagamento);
+    }
+}
